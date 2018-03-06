92bdb828fe3757e0479848f2becaa33e35e56217
Mithun <mithun@vijayasekar.com>
2018-03-05 23:17:06 -0800

    Hello World

---
+++ maxTweeter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{               
+	printf("Hello World\n");
+
+	/*
+    char*   input;        
+
+    scanf("%d",&input);
+
+    printf("%d \n",input);
+    */
+
+    return 0;
+}
